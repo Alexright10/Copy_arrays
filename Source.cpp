335cefe42b88643c76cda9e271b37274b086a5f6
Alexright10 <103695938+Alexright10@users.noreply.github.com>
2025-07-09 17:55:19 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,8 +8,7 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "RUS");
-	// какой-то массив задан
-	// нужно сделать копию и возвратить указатель на нее
+	//  РЅСѓР¶РЅРѕ СЃРґРµР»Р°С‚СЊ РєРѕРїРёСЋ Рё РІРѕР·РІСЂР°С‚РёС‚СЊ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РЅРµРіРѕ
 	int mass[] = { 1,2,3,4,5 };
 
 	double massd[] = { 1.2,2.4,3.5,4.6,5.7,8.9,9.1 };
@@ -18,17 +17,17 @@ void main()
 	
 	Mass obj;
 	int *pmi = obj.copypast(mass, sizeof(mass) / 4);
-	cout << "адрес копии :" << pmi <<  endl;
-	cout << "адрес массива :" << &mass << endl;
+	cout << "Р°РґСЂРµСЃ РєРѕРїРёРё :" << pmi <<  endl;
+	cout << "Р°РґСЂРµСЃ РјР°СЃСЃРёРІР° :" << &mass << endl;
 	obj.get(pmi, sizeof(mass) / 4);
 
 	double* pmd = obj.copypast(massd, sizeof(massd) / 8);
-	cout << "адрес копии :" << pmd <<  endl;
-	cout << "адрес массива :" << &massd << endl;
+	cout << "Р°РґСЂРµСЃ РєРѕРїРёРё :" << pmd <<  endl;
+	cout << "Р°РґСЂРµСЃ РјР°СЃСЃРёРІР° :" << &massd << endl;
 	obj.get(pmd, sizeof(massd) / 8);
 	
 	char* pm = obj.copypast(massch, sizeof(massch));
-	cout << "адрес копии :" << &pm << endl;
-	cout << "адрес массива :" << &massch << endl;
+	cout << "Р°РґСЂРµСЃ РєРѕРїРёРё :" << &pm << endl;
+	cout << "Р°РґСЂРµСЃ РјР°СЃСЃРёРІР° :" << &massch << endl;
 	obj.get(pm, sizeof(massch));
 }
